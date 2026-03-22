0ba347e6eef29bb25801d9a8fb751b20d4de8003
Keiju Asagi <k.asagi@nifty.com>
2026-03-22 21:22:27 +0900

    test(order-resolution): 6.C.7. TEST CASE, DISRUPTED UNIT SWAP

--- server/src/domain/phase/order_resolution_tests.rs
+++ server/src/domain/phase/order_resolution_tests.rs
@@ -875,7 +875,30 @@ fn test_datc_6_c_6() {
 /// A Burgundy - Belgium
 /// None of the units will succeed to move.
 #[test]
-fn test_datc_6_c_7() {}
+fn test_datc_6_c_7() {
+    let mut phase = Phase::new_spring_order(1900, 1);
+    let unit_e_1 = Unit::new_army(Power::England, p("lon"));
+    let unit_e_2 = Unit::new_fleet(Power::England, p("nth"));
+    let order_e_1 = unit_e_1.move_to(p("bel"));
+    let order_e_2 = unit_e_2.convoy(unit_e_1, p("bel"));
+    phase.data.orders.push(order_e_1);
+    phase.data.orders.push(order_e_2);
+    let unit_f_1 = Unit::new_army(Power::France, p("bel"));
+    let unit_f_2 = Unit::new_fleet(Power::France, p("eng"));
+    let unit_f_3 = Unit::new_army(Power::France, p("bur"));
+    let order_f_1 = unit_f_1.move_to(p("lon"));
+    let order_f_2 = unit_f_2.convoy(unit_f_1, p("lon"));
+    let order_f_3 = unit_f_3.move_to(p("bel"));
+    phase.data.orders.push(order_f_1);
+    phase.data.orders.push(order_f_2);
+    phase.data.orders.push(order_f_3);
+    resolve_orders_for_order_phase(&mut phase);
+    assert_eq!(phase.data.orders[0].status, OrderStatus::Failure);
+    assert_eq!(phase.data.orders[1].status, OrderStatus::Valid);
+    assert_eq!(phase.data.orders[2].status, OrderStatus::Failure);
+    assert_eq!(phase.data.orders[3].status, OrderStatus::Valid);
+    assert_eq!(phase.data.orders[4].status, OrderStatus::Failure);
+}
 
 /// 6.C.8. TEST CASE, NO SELF DISLODGEMENT IN DISRUPTED CIRCULAR MOVEMENT
 /// Self dislodgement is prohibited as usual in circular movement.
