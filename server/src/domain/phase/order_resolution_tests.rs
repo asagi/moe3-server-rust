225f3448348e682bb8f176cab3eeba09a062d658
Keiju Asagi <k.asagi@nifty.com>
2026-03-23 21:35:47 +0900

    test(order-resolution): 6.D.19. TEST CASE, EVEN WHEN SURVIVING IS IN ALTERNATIVE WAY

--- server/src/domain/phase/order_resolution_tests.rs
+++ server/src/domain/phase/order_resolution_tests.rs
@@ -1620,7 +1620,22 @@ fn test_datc_6_d_18() {
 /// because one of the supports is of Russian origin.
 /// The support from Black Sea to Ankara will sustain and the fleet in Ankara will be dislodged.
 #[test]
-fn test_datc_6_d_19() {}
+fn test_datc_6_d_19() {
+    let mut phase = Phase::new_spring_order(1900, 1);
+    let unit_r_1 = Unit::new_fleet(Power::Russia, p("con"));
+    let unit_r_2 = Unit::new_fleet(Power::Russia, p("bla"));
+    let unit_r_3 = Unit::new_army(Power::Russia, p("smy"));
+    let unit_t_1 = Unit::new_fleet(Power::Turkey, p("ank"));
+    phase.data.orders.push(unit_r_1.support_move(unit_r_2, p("ank")));
+    phase.data.orders.push(unit_r_2.move_to(p("ank")));
+    phase.data.orders.push(unit_r_3.support_move(unit_t_1, p("con")));
+    phase.data.orders.push(unit_t_1.move_to(p("con")));
+    resolve_orders_for_order_phase(&mut phase);
+    assert_eq!(phase.data.orders[0].status, OrderStatus::Valid);
+    assert_eq!(phase.data.orders[1].status, OrderStatus::Success);
+    assert_eq!(phase.data.orders[2].status, OrderStatus::Valid);
+    assert_eq!(phase.data.orders[3].status, OrderStatus::Dislodged);
+}
 
 /// 6.D.20. TEST CASE, UNIT CANNOT CUT SUPPORT OF ITS OWN COUNTRY
 /// Although this is not mentioned in all rulebooks,
