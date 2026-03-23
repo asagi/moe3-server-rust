41608486853054d0d7cd23b0db30f6ec533a5cce
Keiju Asagi <k.asagi@nifty.com>
2026-03-23 19:37:22 +0900

    test(order-resolution): 変数名の誤りを修正

--- server/src/domain/phase/order_resolution_tests.rs
+++ server/src/domain/phase/order_resolution_tests.rs
@@ -1293,18 +1293,18 @@ fn test_datc_6_d_8() {
 #[test]
 fn test_datc_6_d_9() {
     let mut phase = Phase::new_spring_order(1900, 1);
-    let unit_a_1 = Unit::new_army(Power::Italy, p("ven"));
-    let unit_a_2 = Unit::new_army(Power::Italy, p("tyr"));
-    let order_a_1 = unit_a_1.move_to(p("tri"));
-    let order_a_2 = unit_a_2.support_move(unit_a_1, p("tri"));
+    let unit_i_1 = Unit::new_army(Power::Italy, p("ven"));
+    let unit_i_2 = Unit::new_army(Power::Italy, p("tyr"));
+    let order_i_1 = unit_i_1.move_to(p("tri"));
+    let order_i_2 = unit_i_2.support_move(unit_i_1, p("tri"));
+    phase.data.orders.push(order_i_1);
+    phase.data.orders.push(order_i_2);
+    let unit_a_1 = Unit::new_army(Power::Austria, p("alb"));
+    let unit_a_2 = Unit::new_army(Power::Austria, p("tri"));
+    let order_a_1 = unit_a_1.support_move(unit_a_2, p("ser"));
+    let order_a_2 = unit_a_2.hold();
     phase.data.orders.push(order_a_1);
     phase.data.orders.push(order_a_2);
-    let unit_t_1 = Unit::new_army(Power::Austria, p("alb"));
-    let unit_t_2 = Unit::new_army(Power::Austria, p("tri"));
-    let order_t_1 = unit_t_1.support_move(unit_t_2, p("ser"));
-    let order_t_2 = unit_t_2.hold();
-    phase.data.orders.push(order_t_1);
-    phase.data.orders.push(order_t_2);
     resolve_orders_for_order_phase(&mut phase);
     assert_eq!(phase.data.orders[0].status, OrderStatus::Success);
     assert_eq!(phase.data.orders[1].status, OrderStatus::Valid);
