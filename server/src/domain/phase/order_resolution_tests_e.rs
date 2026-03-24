20a7ff34682ef903cd556d07f440a74162256299
Keiju Asagi <k.asagi@nifty.com>
2026-03-24 23:57:25 +0900

    test(order-resolution): 6.E.7. TEST CASE, NO SELF DISLODGEMENT WITH BELEAGUERED GARRISON

--- server/src/domain/phase/order_resolution_tests_e.rs
+++ server/src/domain/phase/order_resolution_tests_e.rs
@@ -267,7 +267,8 @@ fn test_datc_6_e_6() {
 }
 
 /// 6.E.7. TEST CASE, NO SELF DISLODGEMENT WITH BELEAGUERED GARRISON
-/// An attempt at self dislodgement can be combined with a beleaguered garrison. Such self dislodgment is still not possible.
+/// An attempt at self dislodgement can be combined with a beleaguered garrison.
+/// Such self dislodgment is still not possible.
 ///
 /// England:
 /// F North Sea Hold
@@ -280,9 +281,37 @@ fn test_datc_6_e_6() {
 /// Russia:
 /// F Skagerrak Supports F Norway - North Sea
 /// F Norway - North Sea
-/// Although the Russians beat the German attack (with the support of Yorkshire) and the two Russian fleets are enough to dislodge the fleet in the North Sea, the fleet in the North Sea is not dislodged, since it would not be dislodged if the English fleet in Yorkshire would not give support. This is a typical bug that can happen if a grand winner is calculated of a contested area (instead of calculating every move separately). Of the contested area the North Sea, the Russians are the grand winner with a strength of three, but this doesn't mean that they can advance.
+/// Although the Russians beat the German attack (with the support of Yorkshire)
+/// and the two Russian fleets are enough to dislodge the fleet in the North Sea,
+/// the fleet in the North Sea is not dislodged,
+/// since it would not be dislodged if the English fleet in Yorkshire would not give support.
+/// This is a typical bug that can happen if a grand winner is calculated of a contested area
+/// (instead of calculating every move separately).
+/// Of the contested area the North Sea, the Russians are the grand winner with a strength of three,
+/// but this doesn't mean that they can advance.
 #[test]
-fn test_datc_6_e_7() {}
+fn test_datc_6_e_7() {
+    let mut phase = Phase::new_spring_order(1900, 1);
+    let unit_e_1 = Unit::new_fleet(Power::England, p("nth"));
+    let unit_e_2 = Unit::new_fleet(Power::England, p("yor"));
+    let unit_g_1 = Unit::new_fleet(Power::Germany, p("hol"));
+    let unit_g_2 = Unit::new_fleet(Power::Germany, p("hel"));
+    let unit_r_1 = Unit::new_fleet(Power::Russia, p("ska"));
+    let unit_r_2 = Unit::new_fleet(Power::Russia, p("nwy"));
+    phase.data.orders.push(unit_e_1.hold());
+    phase.data.orders.push(unit_e_2.support_move(unit_r_2, p("nth")));
+    phase.data.orders.push(unit_g_1.support_move(unit_g_2, p("nth")));
+    phase.data.orders.push(unit_g_2.move_to(p("nth")));
+    phase.data.orders.push(unit_r_1.support_move(unit_r_2, p("nth")));
+    phase.data.orders.push(unit_r_2.move_to(p("nth")));
+    resolve_orders_for_order_phase(&mut phase);
+    assert_eq!(phase.data.orders[0].status, OrderStatus::Success);
+    assert_eq!(phase.data.orders[1].status, OrderStatus::Valid);
+    assert_eq!(phase.data.orders[2].status, OrderStatus::Valid);
+    assert_eq!(phase.data.orders[3].status, OrderStatus::Failure);
+    assert_eq!(phase.data.orders[4].status, OrderStatus::Valid);
+    assert_eq!(phase.data.orders[5].status, OrderStatus::Failure);
+}
 
 /// 6.E.8. TEST CASE, NO SELF DISLODGEMENT WITH BELEAGUERED GARRISON AND HEAD-TO-HEAD BATTLE
 /// Similar to the previous test case, but now the beleaguered fleet is also engaged in a head-to-head battle.
